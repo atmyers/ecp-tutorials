41931c6b6b97c2cee4a2f0c7a9796394ddc64dfb
atmyers <atmyers2@gmail.com>
2021-04-14 15:37:43 -0700

    add .gitpod.dockerfile

--- dependencies.sh
+++ dependencies.sh
@@ -2,19 +2,6 @@
 
 set -eu -o pipefail
 
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends\
-    build-essential \
-    g++             \
-    libopenmpi-dev  \
-    openmpi-bin     \
-    paraview        \
-    python3-paraview \
-    ffmpeg \
-    python3 \
-    python3-pip \
-    python3-setuptools
-
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
 sudo update-alternatives --set python /usr/bin/python3
 python -m pip install --upgrade pip
